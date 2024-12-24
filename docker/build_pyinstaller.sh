6f2113dccdfaceb8cfbc4b9676381fca1917cc51
r3t4k3r <g1fl3x228@gmail.com>
2024-12-24 09:35:37 -0500

    remove pyinstaller hook

--- docker/build_pyinstaller.sh
+++ docker/build_pyinstaller.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /app
-/build/.venv/bin/pyinstaller --clean --onedir --additional-hooks-dir /app/hooks --noupx -d noarchive --optimize 2 --strip --bootloader-ignore-signals --hidden-import 'encodings' src/main.py
+/build/.venv/bin/pyinstaller --clean --onedir --noupx -d noarchive --optimize 2 --strip --bootloader-ignore-signals src/main.py
