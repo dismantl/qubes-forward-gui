dfa91cd43478e600cb019b8ea57a87bedcd43b61
r3t4k3r <g1fl3x228@gmail.com>
2025-01-04 01:31:10 -0500

    experimental nuitka support

--- docker/build_nuitka.sh
+++ docker/build_nuitka.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /app
-PATH=/usr/lib/qt6/bin:$PATH python3 -m nuitka --show-progress --static-libpython=yes --module --plugin-enable=pyqt6 --output-dir=out src/main.py # --nofollow-imports
+PATH=/usr/lib/qt6/bin:$PATH /build/.venv/bin/nuitka --show-progress --plugin-enable=pyqt6 --mode=standalone --output-dir=out src/main.py # --nofollow-imports
